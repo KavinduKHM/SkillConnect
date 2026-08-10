86f41a8433b17cdf5c2e9b3061b1e179c1fc0a5b
Kavindu Kalhara <160339120+KavinduKHM@users.noreply.github.com>
2026-08-10 18:07:42 +0530

    Project setup is initiated

---
+++ mobile/App.tsx
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        {/* TODO: Add Navigation */}
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
